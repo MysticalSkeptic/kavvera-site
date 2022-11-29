69770ae63a3ee426fd26b4e8ecd9cf292fb67f96
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2022-11-29 01:25:35 -0600

    bones complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HistoryComponent } from './history/history.component';
+import { CampaignsComponent } from './campaigns/campaigns.component';
+import { LocationsComponent } from './locations/locations.component';
+import { ErrorComponent } from './error/error.component';
+import { MembershipComponent } from './membership/membership.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HistoryComponent,
+    CampaignsComponent,
+    LocationsComponent,
+    ErrorComponent,
+    MembershipComponent
   ],
   imports: [
     BrowserModule
